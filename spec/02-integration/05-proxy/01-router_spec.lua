8f49a0572b0823d320ac0ab21a7aa54da358d0a2
Thibault Charbonnier <thibaultcha@me.com>
2017-10-02 17:40:46 -0700

    fix(postgres) ensure APIs created_at has ms precision
    APIs are retrieved without an `ORDER BY` clause from the datastore when
    building the router. Because of the lack of such a clause in our current
    Cassandra schema, we wish to implement sorting by creation date in our
    business logic, right before building the router.
    
    However, the PostgreSQL default clause of the APIs `created_at` field
    is set to `CURRENT_TIMESTAMP(0)`, which strips ms precision out of the
    timestamp. This changes the default timestamp precision to 3 digits so
    our sorting attribute (`created_at`) can be meaningful.
    
    This fix is targeting 0.11.1, and as such does not introduce a new
    migration to update the default value of `created_at`. This fix will
    only have effect for new database schemas.
    
    A second commit will introduce a migration, which, as per our policy,
    will be targeted for 0.12.0.
    
    Fix #2899

--- spec/02-integration/05-proxy/01-router_spec.lua
+++ spec/02-integration/05-proxy/01-router_spec.lua
@@ -178,6 +178,55 @@ describe("Router", function()
     end)
   end)
 
+  describe("URI regexes order of evaluation", function()
+    setup(function()
+      insert_apis {
+        {
+          name = "api-1",
+          uris = { [[/status/(re)]] },
+          upstream_url = helpers.mock_upstream_url .. "/status/200",
+        },
+        {
+          name = "api-2",
+          uris = { [[/status/(r)]] },
+          upstream_url = helpers.mock_upstream_url .. "/status/200",
+        },
+        {
+          name = "api-3",
+          uris = { [[/(status)]] },
+          upstream_url = helpers.mock_upstream_url .. "/status/200",
+        },
+      }
+
+      helpers.run_migrations()
+      assert(helpers.start_kong({
+        nginx_conf = "spec/fixtures/custom_nginx.template",
+      }))
+    end)
+
+    teardown(function()
+      helpers.stop_kong()
+    end)
+
+    it("depends on created_at field", function()
+      local res = assert(client:send {
+        method  = "GET",
+        path    = "/status/r",
+        headers = { ["kong-debug"] = 1 },
+      })
+      assert.res_status(200, res)
+      assert.equal("api-2", res.headers["kong-api-name"])
+
+      res = assert(client:send {
+        method = "GET",
+        path = "/status/re",
+        headers = { ["kong-debug"] = 1 },
+      })
+      assert.res_status(200, res)
+      assert.equal("api-1", res.headers["kong-api-name"])
+    end)
+  end)
+
   describe("URI arguments (querystring)", function()
 
     setup(function()
